8764c924f6fe45676af50df003800d9339359faa
Jerry Hong <hongjinwei222@gmail.com>
2015-04-08 11:19:40 +0800

    update bat script

--- pic_torrent/取种子.bat
+++ pic_torrent/取种子.bat
@@ -1,5 +1,7 @@
-xcopy /s .\pic\*.* processed\pic\
+xcopy /s .\pic\*.* .\processed\pic\
 java P2t p2t
 xcopy /s .\processed\torrent\*.* .\torrent\
+del /s /f /a /q .\processed\torrent\*
+del /s /f /a /q .\processed\pic\*
 echo ³É¹¦£¡
 PAUSE
--- pic_torrent/种子放入图片中.bat
+++ pic_torrent/种子放入图片中.bat
@@ -1,5 +1,7 @@
-xcopy  /s .\torrent\*.* .\raw\proceesed\torrent\
+xcopy  /s .\torrent\*.* .\raw\torrent\
 java P2t t2p
 xcopy /s .\processed\pic\*.* .\pic
+del /a /f /s /q .\processed\pic\*
+del /a /f /s /q .\raw\torrent\*
 echo ³É¹¦£¡
 PAUSE
